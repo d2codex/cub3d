216ce75edcd3571f442f3ad8c3b2291df9c1cb03
Pauline Froidure <pafroidu@student.42.fr>
2025-12-06 11:54:28 +0100

    feature: quick test in the main, draw a square with only pixels and mlx connection

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 int	main(int argc, char **argv)
 {
 	t_game	game;
+	int		i;
+	int		j;
 
 	if (argc != 2)
 	{
@@ -14,7 +16,23 @@ int	main(int argc, char **argv)
 	init_data(&game);
 	if (parse_map(argv[1], &game.map))
 		return (EXIT_FAILURE);
+	if (init_game_data(&game) != EXIT_SUCCESS)
+		return (EXIT_FAILURE);
+
+	i = 0;
+	while (i < 50)
+	{
+		j = 0;
+		while (j < 50)
+		{
+			draw_pixel_in_buffer(&game, j, i, 0x0000FF00);
+			j++;
+		}
+		i++;
+	}
+	mlx_put_image_to_window(game.mlx, game.win, game.img, 0, 0);
 	print_map_grid(&game.map);
 	free_map(&game.map);
+	mlx_loop(game.mlx);
 	return (EXIT_SUCCESS);
 }
