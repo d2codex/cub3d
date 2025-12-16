564dff44ed3484b30684e800c9e5c84a8b2abc56
d2 <d2.de.sousa@gmail.com>
2025-12-16 01:12:53 +0100

    feat(color): add parse_rgb() and parse_rgb_component() in parse_color.c

---
+++ src/parsing/parse_color.c
@@ -0,0 +1,44 @@
+#include "cub3d.h"
+
+static int	parse_rgb_component(const char *str, int *value)
+{
+	int	rgb_value;
+
+	if (!ft_safe_atoi(str, &rgb_value))
+		return (EXIT_FAILURE);
+	if (rgb_value < 0 || rgb_value > 255)
+		return (EXIT_FAILURE);
+	*value = rgb_value;
+	return (EXIT_SUCCESS);
+}
+
+int	parse_rgb(const char *value, int rgb_values[RGB_SIZE])
+{
+	char	**rgb_strs;
+	int		i;
+
+	rgb_strs = ft_split(value, ',');
+	if (!rgb_strs)
+	{
+		print_errors("RGB: split failed", NULL, NULL);
+		return (EXIT_FAILURE);
+	}
+	i = 0;
+	while (i < RGB_SIZE)
+	{
+		if (!rgb_strs[i] || parse_rgb_component(rgb_strs[i], &rgb_values[i]))
+		{
+			print_errors("RGB value invalid", NULL, NULL);
+			return (free_split(rgb_strs), EXIT_FAILURE);
+		}
+		i++;
+	}
+	if (rgb_strs[RGB_SIZE] != NULL)
+	{
+		print_errors("RGB: Too many values", NULL, NULL);
+		free_split(rgb_strs);
+		return (EXIT_FAILURE);
+	}
+	free_split(rgb_strs);
+	return (EXIT_SUCCESS);
+}
