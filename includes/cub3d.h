fbd8c3a76f0eca854369a90903348cbcf4b088e2
Pauline Froidure <pafroidu@student.42.fr>
2025-12-26 20:24:37 +0100

    chore: norm header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -16,7 +16,7 @@
 /*         DEFINE              */
 /* =========================== */
 
-/* File validation errors */
+/* file validation errors */
 # define ARG_USAGE "Usage: ./cub3D <valid_map.cub>"
 # define FILENAME_NULL "Filename is NULL, please use a valid file"
 # define FILENAME_LENGHT "Filename is too short. Minimum required: x.cub"
@@ -42,7 +42,7 @@
 # define HEADER_MISSING "Missing headers. Expected: 6"
 # define HEADER_TOO_MANY "Too many headers. Expected: 6"
 
-/* Texture-related errors */
+/* texture-related errors */
 # define TEXTURE_TRIM_FAIL "Texture path trim failed"
 # define TEXTURE_INVALID "Texture file invalid or cannot be opened"
 # define TEXTURE_DUPLICATE "Duplicate texture identifier"
@@ -82,19 +82,12 @@
 # define TEXTURE_WIDTH 64
 # define TEXTURE_HEIGHT 64
 # define WINDOWS_MSG "Welcome to CUB3D"
-// minimum wall distance to prevent division by zero in projection
 # define MIN_WALL_DISTANCE 0.001
-// raycasting wall side indicators
 # define VERTICAL_WALL 0
 # define HORIZONTAL_WALL 1
-// texture fallback color for out-of-bounds access
 # define TEXTURE_FALLBACK_COLOR 0x000000
-// movement speed constant (units per frame)
-// smaller = slower, larger = faster
 # define MOVE_SPEED 0.05
-// rotation speed in radians per frame
 # define ROT_SPEED 0.05
-// mouse sensitivity for rotation (radians per pixel)
 # define MOUSE_SENSITIVITY 0.00005
 
 /* =========================== */
@@ -116,17 +109,17 @@ typedef enum e_header_type
 /* map structure - stores parsed .cub file data */
 typedef struct s_map
 {
-	char	**grid;			// 2d char array representing map layout
-	int		width;			// map width (max number of columns)
-	int		height;			// map height (number of lines)
+	char	**grid;
+	int		width;
+	int		height;
 	int		floor_color[RGB_SIZE];
 	int		ceiling_color[RGB_SIZE];
 	char	*tex_paths[TEX_SIZE];
 	bool	id_set[HEADER_SIZE];
-	int		map_start_line; //where map lines start
+	int		map_start_line;
 }	t_map;
 
-/* Tracks which keys are currently pressed */
+/* tracks which keys are currently pressed */
 typedef struct s_keys
 {
 	bool	w_pressed;
@@ -137,7 +130,7 @@ typedef struct s_keys
 	bool	right_arrow_pressed;
 }	t_keys;
 
-/* Represents the player's state in the game */
+/* represents the player's state in the game */
 typedef struct s_player
 {
 	double	pos_x;
@@ -148,42 +141,37 @@ typedef struct s_player
 	double	plane_y;
 }	t_player;
 
-/* Texture data loaded from .xpm files */
+/* texture data loaded from .xpm files */
 typedef struct s_texture
 {
-	void	*img;			// MLX image pointer (from mlx_xpm_file_to_image)
-	char	*addr;			// Pixel data address (from mlx_get_data_addr)
-	int		width;			// Texture width in pixels
-	int		height;			// Texture height in pixels
-	int		bpp;			// Bits per pixel
-	int		line_len;		// Number of bytes per line
-	int		endian;			// Endian format
+	void	*img;
+	char	*addr;
+	int		width;
+	int		height;
+	int		bpp;
+	int		line_len;
+	int		endian;
 }	t_texture;
 
-/* Encapsulates the entire game state and resources */
+/* encapsulates the entire game state and resources */
 typedef struct s_game
 {
-	// === MLX ===
-	void		*mlx;		// pointer to the MLX connection (init with mlx_init)
-	void		*win;		// pointer to the window (created with mlx_new_window)
-	// === Image buffer ===
-	void		*img;		// pointer to an image used for drawing pixels
-	char		*img_addr;	// memory address of the image pixels (from mlx_get_data_addr)
-	int			img_bpp;		// bits per pixel (from mlx_get_data_addr)
-	int			img_line_len;	// number of bytes in a line of the image (from mlx_get_data_addr)
-	int			img_endian;	// endian format of the image (from mlx_get_data_addr)
-	// === Assets ===
-	t_texture	textures[4];	// array of 4 loaded textures [0]=NO, [1]=SO, [2]=WE, [3]=EA
-	// === Game state ===
-	t_player	player;		// player data (position, direction, camera plane)
-	t_map		map;			// map data (grid, size, textures paths, colors)
-	t_keys		keys;		// tracks which keys are currently pressed
-		// === Mouse ===
-	int			last_mouse_x;	// last mouse X position for delta calculation
-	int			last_mouse_y;	// last mouse Y position for delta calculation
+	void		*mlx;
+	void		*win;
+	void		*img;
+	char		*img_addr;
+	int			img_bpp;
+	int			img_line_len;
+	int			img_endian;
+	t_texture	textures[4];
+	t_player	player;
+	t_map		map;
+	t_keys		keys;
+	int			last_mouse_x;
+	int			last_mouse_y;
 }	t_game;
 
-/* Player orientation struct for the look up table */
+/* player orientation struct for the look up table */
 typedef struct s_orientation
 {
 	char	c;
@@ -201,39 +189,39 @@ typedef struct e_header_entry
 	size_t			len;
 }	t_header_entry;
 
-/* Key binding struct for mapping keys to actions */
+/* key binding struct for mapping keys to actions */
 typedef struct s_key_binding
 {
-	int		keycode; // X11 keycode for this specific key (ex: XK_w, XK_Left)
-	void	(*action)(t_game *); // function pointer to the action that should be executed
-	bool	*flag_ptr; // Pointer to the boolean flag that represents whether this key is currently pressed (true) or released (false)
+	int		keycode;
+	void	(*action)(t_game *);
+	bool	*flag_ptr;
 }	t_key_binding;
 
-/* Ray structure for raycasting calculation */
+/* ray structure for raycasting calculation */
 typedef struct s_ray
 {
-	double	dir_x;			// X component of ray direction
-	double	dir_y;			// Y component of ray direction
-	int		map_x;			// Current grid X coordinate
-	int		map_y;			// Current grid Y coordinate
-	double	side_dist_x;	// Distance ray travels to next X grid line
-	double	side_dist_y;	// Distance ray travels to next Y grid line
-	double	delta_dist_x;	// Distance between X grid lines along ray
-	double	delta_dist_y;	// Distance between Y grid lines along ray
-	int		step_x;			// Step direction in X (either +1 or -1)
-	int		step_y;			// Step direction in Y (either +1 or -1)
-	int		side;			// Was wall hit on X-side (0) or Y-side (1)?
+	double	dir_x;
+	double	dir_y;
+	int		map_x;
+	int		map_y;
+	double	side_dist_x;
+	double	side_dist_y;
+	double	delta_dist_x;
+	double	delta_dist_y;
+	int		step_x;
+	int		step_y;
+	int		side;
 }	t_ray;
 
-/* Ray result structure containing all raycasting outputs */
+/* ray result structure containing all raycasting outputs */
 typedef struct s_ray_result
 {
-	double	wall_dist;	// Perpendicular distance to wall
-	int		wall_dir;	// Wall direction (NO/SO/WE/EA)
-	double	wall_x;		// Hit position on wall (0.0 to 1.0)
+	double	wall_dist;
+	int		wall_dir;
+	double	wall_x;
 }	t_ray_result;
 
-/* Column drawing parameters for rendering */
+/* column drawing parameters for rendering */
 typedef struct s_col
 {
 	int	x;
@@ -242,7 +230,7 @@ typedef struct s_col
 	int	color;
 }	t_col;
 
-/* Drawing info for a complete column (ceiling + wall + floor) */
+/* drawing info for a complete column (ceiling + wall + floor) */
 typedef struct s_draw_info
 {
 	int		x;
@@ -260,131 +248,136 @@ typedef struct s_draw_info
 /* =========================== */
 
 /* events handlers.c */
-int				handle_keypress(int keycode, void *param);
-int				handle_keyrelease(int keycode, void *param);
-int				handle_close(void *param);
-int				game_loop(void *param);
-t_key_binding	*get_key_bindings(t_game *game);
+int						handle_keypress(int keycode, void *param);
+int						handle_keyrelease(int keycode, void *param);
+int						handle_close(void *param);
+int						game_loop(void *param);
+t_key_binding			*get_key_bindings(t_game *game);
 
 /* mouse_handler.c */
-int				handle_mouse_move(int x, int y, void *param);
-void			apply_camera_rotation(t_game *game, double angle);
+int						handle_mouse_move(int x, int y, void *param);
+void					apply_camera_rotation(t_game *game, double angle);
 
 /* hooks.c */
-void			setup_hooks(t_game *game);
+void					setup_hooks(t_game *game);
 
 /* player_actions_rotate.c */
-void			rotate_left(t_game *game);
-void			rotate_right(t_game *game);
+void					rotate_left(t_game *game);
+void					rotate_right(t_game *game);
 
 /* player_actions_move.c */
-void			move_forward(t_game *game);
-void			move_backward(t_game *game);
-void			strafe_left(t_game *game);
-void			strafe_right(t_game *game);
+void					move_forward(t_game *game);
+void					move_backward(t_game *game);
+void					strafe_left(t_game *game);
+void					strafe_right(t_game *game);
 
 /* =========================== */
 /*           INIT              */
 /* =========================== */
 
 /* init_data.c */
-void			init_data(t_game *game);
-int				load_and_validate_map(char *path, t_game *game);
+void					init_data(t_game *game);
+int						load_and_validate_map(char *path, t_game *game);
 
 /* init_mlx.c */
-int				init_game_data(t_game *game);
+int						init_game_data(t_game *game);
 
 /* init_textures.c */
-int				init_textures(t_game *game);
+int						init_textures(t_game *game);
 
 /* =========================== */
 /*         PARSING             */
 /* =========================== */
 
 /* check_headers.c */
-int				check_header_count(const char *path);
+int						check_header_count(const char *path);
 
 /* file_validations.c */
-int				validate_argument(char *filename);
+int						validate_argument(char *filename);
 
 /* header_table.c */
 const t_header_entry	*get_header_entry(const char *line);
 
 /* header_utils.c */
-void			next_line(char **line, int fd, int *i);
-bool			line_is_empty(char *line);
+void					next_line(char **line, int fd, int *i);
+bool					line_is_empty(char *line);
 
 /* parse_header.c */
-int				parse_header(const char *path, t_map *map);
+int						parse_header(const char *path, t_map *map);
 
 /* parse_header_line.c */
-int				parse_header_line(t_map *map, char *line);
+int						parse_header_line(t_map *map, char *line);
 
 /*parse_map.c */
-int				parse_map(const char *path, t_map *map);
+int						parse_map(const char *path, t_map *map);
 
 /*parse_map_utils.c */
-int				max_int(int a, int b);
-int				open_cub_file(const char *path);
-void			print_map_grid(t_map *map);
-void			free_map_grid(t_map *map);
-void			free_partial_grid(t_map *map, int filled_rows);
+int						max_int(int a, int b);
+int						open_cub_file(const char *path);
+void					print_map_grid(t_map *map);
+void					free_map_grid(t_map *map);
+void					free_partial_grid(t_map *map, int filled_rows);
 
 /* parse_rgb.c */
-int				parse_rgb(const char *value, int rgb_values[RGB_SIZE]);
+int						parse_rgb(const char *value, int rgb_values[RGB_SIZE]);
 
 /* player_setup.c */
-int				init_player(t_game *game);
+int						init_player(t_game *game);
 
 /* player_setup_utils.c */
-void			print_player_info(t_player *player);
+void					print_player_info(t_player *player);
 
 /* validate_map.c */
-int				check_valid_map(t_map *map);
+int						check_valid_map(t_map *map);
 
 /* =========================== */
 /*           RENDERS           */
 /* =========================== */
 
 /* draw_pixels.c*/
-void			draw_pixel_in_buffer(t_game *game, int x, int y, int color);
+void					draw_pixel_in_buffer(t_game *game, int x, int y,
+							int color);
 
 /* =========================== */
 /*         RAYCAST             */
 /* =========================== */
 
 /* dda.c */
-t_ray_result	cast_ray(t_game *game, double ray_dir_x, double ray_dir_y);
+t_ray_result			cast_ray(t_game *game, double ray_dir_x,
+							double ray_dir_y);
 
 /* dda_utils.c */
-bool			check_hit(t_game *game, t_ray *ray);
-double			calculate_wall_x(t_ray *ray, double pos_x, double pos_y,
-					double wall_dist);
-int				get_wall_direction(t_ray *ray);
+bool					check_hit(t_game *game, t_ray *ray);
+double					calculate_wall_x(t_ray *ray, double pos_x, double pos_y,
+							double wall_dist);
+int						get_wall_direction(t_ray *ray);
 
 /* raycast_utils.c */
-int				calculate_tex_x(double wall_x);
-int				get_texture_pixel(t_texture *texture, int tex_x, int tex_y);
-void			draw_textured_wall_slice(t_game *game, t_draw_info info);
+int						calculate_tex_x(double wall_x);
+int						get_texture_pixel(t_texture *texture, int tex_x,
+							int tex_y);
+void					draw_textured_wall_slice(t_game *game,
+							t_draw_info info);
 
 /* raycast.c */
-void			render_frame(t_game *game);
+void					render_frame(t_game *game);
 
 /* =========================== */
 /*           UTILS             */
 /* =========================== */
 
 /* ascii art */
-void			print_ascii_art_hello(void);
+void					print_ascii_art_hello(void);
 
 /* cleanup_exit.c */
-void			free_t_map(t_map *map);
-void			cleanup_exit(t_game *game);
+void					cleanup_textures(t_game *game);
+void					free_t_map(t_map *map);
+void					cleanup_exit(t_game *game);
 
 /* print_errors.c */
-void			print_errors(char *p1, char *p2, char *p3);
+void					print_errors(char *p1, char *p2, char *p3);
 
 /* rgb_tab_to_int.c */
-int				rgb_tab_to_int(int rgb[RGB_SIZE]);
+int						rgb_tab_to_int(int rgb[RGB_SIZE]);
 
 #endif
